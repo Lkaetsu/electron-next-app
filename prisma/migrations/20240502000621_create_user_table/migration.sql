f7e07ea56c77b7b7ff49e4d834482f1cfbe0a6bb
Lkaetsu <leonardo.kaetsu9@gmail.com>
2024-05-01 21:10:31 -0300

    electron-next-app 0.2.4
    #Changes:
    - Fixed most, if not all, problems related to the enviroment.

--- prisma/migrations/20240502000621_create_user_table/migration.sql
+++ prisma/migrations/20240502000621_create_user_table/migration.sql
@@ -1,18 +1,19 @@
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Profile` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `password` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
     `userId` INTEGER NOT NULL,
 
+    UNIQUE INDEX `Profile_email_key`(`email`),
     UNIQUE INDEX `Profile_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
